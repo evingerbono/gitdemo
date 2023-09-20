0417cc924f39f8afaddb48aea358d9ad7a9912f0
evingerbono <bono0823@gmail.com>
2023-09-20 12:56:52 +0200

    alap_teszt

---
+++ src/teszt/Teszt.java
@@ -0,0 +1,21 @@
+
+package teszt;
+
+public class Teszt {
+
+   
+    public static void main(String[] args) {
+        metodus();
+        for (int i = 0; i < 10; i++) {
+            
+        }
+        if (true) {
+            
+        }
+    }
+
+    private static void metodus() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
